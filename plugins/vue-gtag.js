250f16d1678b2fdc476124b9af8249e3c9d09527
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2021-04-26 17:50:59 -0500

    actualizado plugin gtm

--- plugins/vue-gtag.js
+++ plugins/vue-gtag.js
@@ -1,18 +1,36 @@
-import Vue from 'vue';
-import VueGtag from 'vue-gtag';
+import Vue from "vue";
+import VueGtag from "vue-gtag";
+
+Vue.use(VueGtag, {
+  config: { 
+    id: "GTM-NXF9K2Z",
+    params: {
+      send_page_view: false
+    }
+  }
+});
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
 
 
-export default ({ app }) => {
-    const getGDPR = localStorage.getItem('GDPR:accepted');
 
-    Vue.use(VueGtag, {
-        config: { id: 'GTM-NXF9K2Z' },
-        appName: 'Sumaq',
-        bootstrap: getGDPR === 'true',
-        enabled: getGDPR === 'true',
-        pageTrackerScreenviewEnabled: true
-    }, app.router);
-}
+// import Vue from 'vue';
+// import VueGtag from 'vue-gtag';
+
+
+// export default ({ app }) => {
+//     const getGDPR = localStorage.getItem('GDPR:accepted');
+
+//     Vue.use(VueGtag, {
+//         config: { id: 'GTM-NXF9K2Z' },
+//         appName: 'Sumaq',
+//         bootstrap: getGDPR === 'true',
+//         enabled: getGDPR === 'true',
+//         pageTrackerScreenviewEnabled: true
+//     }, app.router);
+// }
 
 // import Vue from "vue";
 // import VueGtag from "vue-gtag";
