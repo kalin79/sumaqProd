ea8b3d9c71895b87f73e2cbf29d5809b9082a59a
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2021-06-03 18:27:54 -0500

    ajustes

--- components/Cart/Pago.vue
+++ components/Cart/Pago.vue
@@ -1142,7 +1142,7 @@ export default {
                          .boxDetail
                               padding: 0 0 0 1.5rem
                               @media screen and (min-width: 992px)
-                                   width: 270px
+                                   width: 267px
                               @media screen and (min-width: 1200px)
                                    width: 300px
                               h2
